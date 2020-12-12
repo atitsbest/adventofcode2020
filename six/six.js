d2aa78beb1e899cc2fbc8c898f98c5eaba5f7535
Meissner <smeissner@dccsintra.net>
2020-12-12 19:57:06 +0100

    day six, pat 2

--- six/six.js
+++ six/six.js
@@ -3,14 +3,32 @@ fs = require("fs");
 const input = fs
   .readFileSync("./input.txt", "utf-8")
   .split(/^\r\n/gm)
-  .map((l) => l.trim().replace(/\r\n/gm, ""));
+  .map((l) => l.trim().split("\r\n"));
+
+console.log(input);
 
 function answerCount(group) {
-  const result = group.split("").reduce((p, c) => ({ ...p, [c]: true }), {});
+  const result = group
+    .join("")
+    .split("")
+    .reduce((p, c) => ({ ...p, [c]: true }), {});
   return Object.keys(result).length;
+}
+
+function answerCount2(group) {
+  const gc = group.length;
+  const result = group
+    .join("")
+    .split("")
+    .reduce((p, c) => ({ ...p, [c]: (p[c] || 0) + 1 }), {});
+  return Object.keys(result).reduce(
+    (p, c) => (p += result[c] === gc ? 1 : 0),
+    0
+  );
 }
 
 console.log(
   "Result",
-  input.map(answerCount).reduce((p, c) => p + c, 0)
+  input.map(answerCount).reduce((p, c) => p + c, 0),
+  input.map(answerCount2).reduce((p, c) => p + c, 0)
 );
